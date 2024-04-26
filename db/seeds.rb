33309c01d78d3e6a15381d8a984f1d4baaaa7bfc
Chris Collis <theotteryears@me.com>
2024-04-26 16:40:30 +0200

    Frontend work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+puts "Cleaning up DB"
+
+Movie.destroy_all
+
+puts "Creating movies..."
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
@@ -6,4 +12,7 @@ Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells
 
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-Movie.create(title: "Arrival", overview: "Aliens blah blah blah", poster_url: "", rating: 10.0)
+Movie.create(title: "Arrival", overview: "Aliens blah blah blah", poster_url: "https://i5.walmartimages.com/seo/Arrival-DVD-Paramount-Sci-Fi-Fantasy_23651a1a-12a8-4d90-be1e-25956362367f.1de8dcfb30e45885105b32aaabf838f6.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF", rating: 10.0)
+
+
+puts "Finished creating movies!"
