68ca76fe85d6b1a6f9663d8920b13d4462f40c81
Chris Collis <theotteryears@me.com>
2024-04-26 13:50:49 +0200

    Add delete button for list item

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   root "lists#index"
 
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: :destroy
